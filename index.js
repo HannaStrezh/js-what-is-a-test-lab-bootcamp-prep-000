ab6024a2823402b59fde249166a035dfd6eb0070
Hanna Strezh <hannastrezh@gmail.com>
2018-06-25 19:22:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // write your code below!
 var name = "Susan"
-var height = 74
+var height1 = 74
+var height2 =40
+if(height1<height2)
 var message = ` ${name}is ${height} inches tall`
 module.exports = {name, height, message}
