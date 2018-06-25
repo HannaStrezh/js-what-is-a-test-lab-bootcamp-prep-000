981fddb1f8ae8f57ed5d73e80904fc072496d0b6
Hanna Strezh <hannastrezh@gmail.com>
2018-06-25 19:07:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 var name = "Susan"
 var height = 74
 var message = ` ${name}is ${height} inches tall`
-      expect(index.height).toBeLessThan(40)
+      expect(index.height).toBeLessThan(40);
 module.exports = {name, height, message}
