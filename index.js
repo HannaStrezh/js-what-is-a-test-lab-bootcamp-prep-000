f37ba88effa7803c666f68be8cca989215422da3
Hanna Strezh <hannastrezh@gmail.com>
2018-06-25 19:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 // write your code below!
 var name = "Susan"
-var height = "40"
+var height = 74
 var message = ` ${name}is ${height} inches tall`
-describe("Height", () => {
-    it("is less than 40", () => {
       expect(index.height).toBeLessThan(40)
-    })
-  })
 module.exports = {name, height, message}
